49742dde411ac4e0cd19c0ff9a0b34cb09a80485
sriram-snow <108343505+sriram-snow@users.noreply.github.com>
2022-06-30 16:24:12 +0530

    Update streamlit_app.py
    added df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
+import pandas as pd
 streamlit.title('Business is business')
 
 streamlit.header('Organs left to smuggle')
 streamlit.text('Kidney dialysis')
 streamlit.text('Blockaged heart')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
