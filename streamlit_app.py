ba41e3b7a12496f16eef7939b52757441283d14f
sriram-snow <108343505+sriram-snow@users.noreply.github.com>
2022-07-08 15:55:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit
-# import pandas as pd
+# import pandas
 streamlit.title('Business is business')
 
 streamlit.header('Organs left to smuggle')
 streamlit.text('Kidney dialysis')
 streamlit.text('Blockaged heart')
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -21,7 +21,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 # import requests
 fruityvice_repsonse = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pd.json_normalize(fruityvice_repsonse.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_repsonse.json())
 streamlit.dataframe(fruityvice_normalized)
 # dont run anything above
 streamlit.stop()
